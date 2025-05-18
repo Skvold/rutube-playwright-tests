87033d075f46a50c938de17ea651ad19e1547db1
Skvold <zlostniy85@gmail.com>
2025-05-18 16:31:24 +0300

    add main page tests

--- tests/pages/MainPage.ts
+++ tests/pages/MainPage.ts
@@ -1,7 +1,28 @@
-import { Page } from '@playwright/test';
+import { expect, Locator, Page } from '@playwright/test';
 import { BasePage } from './BasePage';
 export class MainPage extends BasePage {
+  private readonly headerLocator: Locator;
+  private readonly caregoriesTabsLocator: Locator;
+  private readonly menuLocator: Locator;
+
+  constructor(page: Page) {
+    super(page);
+    this.headerLocator = this.page.getByText(
+      'RUTUBEДобавитьУведомленияПереключить на светлую темуВход и регистрация',
+    );
+    this.caregoriesTabsLocator = this.page.locator('.wdp-tabs-module__tabs');
+    this.menuLocator = this.page.getByLabel('Облегченная панель навигации');
+  }
   async open() {
     this.page.goto('https://rutube.ru/');
+  }
+  async headerHasCorrectAriaSnapshot() {
+    await expect(this.headerLocator).toMatchAriaSnapshot();
+  }
+  async caregoriesTabsHasCorrectAriaSnapshot() {
+    await expect(this.caregoriesTabsLocator).toMatchAriaSnapshot();
+  }
+  async menuHasCorrectAriaSnapshot() {
+    await expect(this.menuLocator).toMatchAriaSnapshot();
   }
 }
