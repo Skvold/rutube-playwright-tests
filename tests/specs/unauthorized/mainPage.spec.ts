43dd4467228459eca223545d651145b3f86a92af
Skvold <zlostniy85@gmail.com>
2025-05-18 16:38:08 +0300

    add main page fixture

--- tests/specs/unauthorized/mainPage.spec.ts
+++ tests/specs/unauthorized/mainPage.spec.ts
@@ -1,21 +1,12 @@
-import test from '@playwright/test';
+import { test, expect } from '../../fixtures/fixtures';
 import { MainPage } from '../../pages/MainPage';
 
-test('Проверка доступности элементов хедера', async ({ page }) => {
-  const mainPage = new MainPage(page);
-  await mainPage.open();
-  await mainPage.closeCookiesAlert();
+test('Проверка доступности элементов хедера', async ({ mainPage }) => {
   await mainPage.headerHasCorrectAriaSnapshot();
 });
-test('Проверка доступности табов категорий', async ({ page }) => {
-  const mainPage = new MainPage(page);
-  await mainPage.open();
-  await mainPage.closeCookiesAlert();
+test('Проверка доступности табов категорий', async ({ mainPage }) => {
   await mainPage.caregoriesTabsHasCorrectAriaSnapshot();
 });
-test('Проверка доступности элементов бокового меню', async ({ page }) => {
-  const mainPage = new MainPage(page);
-  await mainPage.open();
-  await mainPage.closeCookiesAlert();
+test('Проверка доступности элементов бокового меню', async ({ mainPage }) => {
   await mainPage.menuHasCorrectAriaSnapshot();
 });
