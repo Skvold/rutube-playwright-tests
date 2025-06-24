3155c3168e17001e277ff3340a8a45c2127e802e
Skvold <zlostniy85@gmail.com>
2025-06-24 22:16:46 +0300

    fix snapshots

--- playwright.config.ts
+++ playwright.config.ts
@@ -44,7 +44,7 @@ export default defineConfig({
     {
       name: 'chromium authorized',
       use: { ...devices['Desktop Chrome'], storageState: 'tests/playwright/.auth/user.json' },
-      dependencies: ['auth'],
+      // dependencies: ['auth'],
       testDir: 'tests/specs/authorized',
       snapshotPathTemplate: 'tests/snapshots/authorized/{testFilePath}/{arg}{ext}',
     },
@@ -54,10 +54,10 @@ export default defineConfig({
       testDir: 'tests/specs/unauthorized',
       snapshotPathTemplate: 'tests/snapshots/unauthorized/{testFilePath}/{arg}{ext}',
     },
-    {
-      name: 'auth',
-      testMatch: 'tests/specs/authTest.spec.ts',
-    },
+    // {
+    //   name: 'auth',
+    //   testMatch: 'tests/specs/authTest.spec.ts',
+    // },
     /* Test against mobile viewports. */
     // {
     //   name: 'Mobile Chrome',
